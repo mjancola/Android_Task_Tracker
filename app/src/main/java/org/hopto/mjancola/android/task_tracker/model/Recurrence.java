8b22a1b3b910fecfe32fc9d0b3d70ed422f5e98e
Mike Jancola <mjancola@Mikes-MacBook-Pro.local>
2016-02-28 23:33:59 -0800

    clean up and add new model representation of recurring interval

---
+++ app/src/main/java/org/hopto/mjancola/android/task_tracker/model/Recurrence.java
@@ -0,0 +1,7 @@
+package org.hopto.mjancola.android.task_tracker.model;
+
+/**
+ * Created by mjancola on 2/19/16.
+ */
+public class Recurrence {
+}
---
+++ app/src/test/java/org/hopto/mjancola/android/task_tracker/model/RecurrenceTest.java
@@ -0,0 +1,7 @@
+package org.hopto.mjancola.android.task_tracker.model;
+
+/**
+ * Created by mjancola on 2/20/16.
+ */
+public class RecurrenceTest {
+}

9a970fd36d6c2e9b757608b46c12614bd4f0c6e5
Mike Jancola <mjancola@Mikes-MacBook-Pro.local>
2016-02-28 23:40:01 -0800

    add unit tests and build changes for tests

--- app/src/main/java/org/hopto/mjancola/android/task_tracker/model/Recurrence.java
+++ app/src/main/java/org/hopto/mjancola/android/task_tracker/model/Recurrence.java
@@ -1,7 +1,139 @@
 package org.hopto.mjancola.android.task_tracker.model;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 /**
  * Created by mjancola on 2/19/16.
  */
+
+
 public class Recurrence {
+
+    // NOTE, uses Java Calendar's DayOfWeek (Sun=1 -> Sat=7)
+
+    public static enum RecurrenceType {Daily, Weekly, Monthly, Yearly, Days, Weeks, Months, Years};
+
+    private RecurrenceType type;
+    private ArrayList<Integer> value;
+    private long lastCompleted;
+    private Long testTimeMS; // MS since epoch - cached here to facilitate testing
+
+    /**
+     * Constructor for Recurrence type
+     *
+     * @param type enum Recurrence type
+     * @param value depends on type
+     *   Type daily -> value is ignored
+     *   Type weekly -> array of DayOfWeek enum
+     *   Type monthly-> array of day of month (note, value exceeding a given month equals last day of month)
+     *   Type yearly -> array of day of year (represented numerically as MMDD)
+     *   Type Days, Weeks, Months, Years -> array(0) is integer number of items, for example every 3 months
+     * @param lastCompleted long value ms since epoch, defaults to 0 if not specified
+     */
+    public Recurrence(RecurrenceType type, ArrayList<Integer> value, Long lastCompleted){
+        if ((type == null) || (!type.equals(RecurrenceType.Daily) && value == null)) {
+            throw new IllegalArgumentException("type and value required");
+        }
+
+        this.type = type;
+        this.value = value;
+        this.testTimeMS = null;
+        if (lastCompleted != null) {
+            this.lastCompleted = lastCompleted;
+        } else {
+            this.lastCompleted = 0l;
+        }
+    }
+
+    // package private for testing
+    void setTestTimeMS(long timeMS) {
+        this.testTimeMS = timeMS;
+    }
+
+    /**
+     * get system time, if test time not set
+     * @return long time in MS
+     */
+    private long getTestTimeMS() {
+        if (this.testTimeMS != null) {
+            return this.testTimeMS;
+        } else {
+            return System.currentTimeMillis();
+        }
+    }
+
+    /**
+     * calculate how many MS until due
+     * returns negative if overdue
+     * @return
+     */
+    public Long dueDeltaMS() {
+        Long now = getTestTimeMS(); // System.currentTimeMillis();
+        Calendar cal = Calendar.getInstance();
+        Long dueDate = 0l;
+
+        switch (type) {
+            case Weekly:
+                cal.setTimeInMillis(lastCompleted);
+                int lastDay = cal.get(Calendar.DAY_OF_WEEK);
+                int daysDueAfterLast;
+                // look for next day
+                for (daysDueAfterLast=1; daysDueAfterLast<8; daysDueAfterLast++) {  // max of 7 days available
+                    if (this.getValue().contains((lastDay+daysDueAfterLast) & 7)) {
+                        // found next day
+                        break;
+                    }
+                }
+                cal.add(Calendar.DAY_OF_YEAR, daysDueAfterLast);
+                cal.set(Calendar.HOUR, 0);  // set the time to 12:01a
+                cal.set(Calendar.MINUTE, 1);
+                dueDate = cal.getTimeInMillis();
+                break;
+
+            case Daily:
+                cal.setTimeInMillis(lastCompleted);
+                cal.add(Calendar.DAY_OF_YEAR, 1);  // by definition, daily is every day
+                dueDate = cal.getTimeInMillis();
+                break;
+            case Years:
+                cal.setTimeInMillis(lastCompleted);
+                cal.add(Calendar.YEAR, value.get(0));
+                dueDate = cal.getTimeInMillis();
+                break;
+            case Months:
+                cal.setTimeInMillis(lastCompleted);
+                cal.add(Calendar.MONTH, value.get(0));
+                dueDate = cal.getTimeInMillis();
+                break;
+            case Weeks:
+                cal.setTimeInMillis(lastCompleted);
+                cal.add(Calendar.WEEK_OF_YEAR, value.get(0));
+                dueDate = cal.getTimeInMillis();
+                break;
+            case Days:
+                cal.setTimeInMillis(lastCompleted);
+                cal.add(Calendar.DAY_OF_YEAR, value.get(0));
+                dueDate = cal.getTimeInMillis();
+                break;
+            default:
+                throw new IllegalArgumentException("unrecognized recurrence type");
+
+        }
+
+        return dueDate - now;
+    }
+
+    public Recurrence.RecurrenceType getType() {
+        return type;
+    }
+
+    public ArrayList<Integer> getValue() {
+        return value;
+    }
+
+    public Long getLastCompleted() {
+        return lastCompleted;
+    }
+
 }
--- app/src/test/java/org/hopto/mjancola/android/task_tracker/model/RecurrenceTest.java
+++ app/src/test/java/org/hopto/mjancola/android/task_tracker/model/RecurrenceTest.java
@@ -1,7 +1,265 @@
 package org.hopto.mjancola.android.task_tracker.model;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by mjancola on 2/20/16.
  */
 public class RecurrenceTest {
+
+    private static final Long TEST_COMPLETED = 99l;
+    private static final Long TEST_NEVER_COMPLETED = 0l;
+    private static final Integer TEST_VALUE_ONE = 1;
+    private static final int TEST_DAYOFWEEK = Calendar.MONDAY;
+    private static final Integer TEST_DAYOFMONTH = 1;
+    private static final Integer TEST_DAYOFYEAR = 0101;  // jan 1
+    private static final Long TEST_ONE_HOUR = 1000l*60l*60l;
+    private static ArrayList<Integer> TEST_ARRAY_1 = new ArrayList<Integer>();
+
+    @Before
+    public void setUp() {
+        TEST_ARRAY_1.add(TEST_VALUE_ONE);
+    }
+
+    @Test
+    public void testNotNull() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, null, null);
+        assertNotNull("new recurrence shall not be null", testRecurrence);
+    }
+
+    @Test
+    public void testValidConstruction() {
+        Recurrence testRecurrence;
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, null, TEST_COMPLETED);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, TEST_ARRAY_1, TEST_COMPLETED);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weekly, TEST_ARRAY_1, TEST_COMPLETED);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, TEST_ARRAY_1, TEST_COMPLETED);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Yearly, TEST_ARRAY_1, TEST_COMPLETED);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Days, TEST_ARRAY_1, TEST_COMPLETED);
+        assertNotNull("valid contruction failed", testRecurrence);
+    }
+
+    @Test
+    public void testEmptyLastCompleted() {
+        Recurrence testRecurrence;
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, null, null);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, TEST_ARRAY_1, null);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weekly, TEST_ARRAY_1, null);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, TEST_ARRAY_1, null);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Yearly, TEST_ARRAY_1, null);
+        testRecurrence = new Recurrence(Recurrence.RecurrenceType.Days, TEST_ARRAY_1, null);
+        assertNotNull("contruction with empty last completed should be ok", testRecurrence);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNullType() {
+        Recurrence testRecurrence = new Recurrence(null, TEST_ARRAY_1, TEST_COMPLETED);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNullValue() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, null, TEST_COMPLETED);
+    }
+
+    @Test
+    public void testNullValueOkForDaily() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, null, TEST_COMPLETED);
+        assertNotNull("Daily recurrence does not require a value", testRecurrence);
+    }
+
+    @Test
+    public void testGetType() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, TEST_ARRAY_1, TEST_COMPLETED);
+        assertEquals("getType should return type", Recurrence.RecurrenceType.Monthly, testRecurrence.getType());
+    }
+
+    @Test
+    public void testGetValue() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, TEST_ARRAY_1, TEST_COMPLETED);
+        assertTrue("getValue should return value", TEST_ARRAY_1.equals(testRecurrence.getValue()));
+    }
+
+    @Test
+    public void testGetCompleted() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, TEST_ARRAY_1, TEST_COMPLETED);
+        assertEquals("getCompleted should return last completed", TEST_COMPLETED, testRecurrence.getLastCompleted());
+    }
+
+    @Test
+    public void testNullLastCompleted() {
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Monthly, TEST_ARRAY_1, null);
+        assertEquals("getCompleted should return 0 if not specified", TEST_NEVER_COMPLETED, testRecurrence.getLastCompleted());
+    }
+
+    @Test
+    public void testComplete() {
+
+    }
+
+    @Test
+    public void testDailyOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -25);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, null, prevMS);
+        assertTrue("daily overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testDailyNotOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -23);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Daily, null, prevMS);
+        assertFalse("daily overdue should NOT be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+
+
+    @Test
+    public void testWeeklyOverdue() {
+        // due Mon/Thurs
+        // set date to Thurs, set last completed to Tues (AFTER NOW, just to make sure we are not looking at times)
+        Calendar testDate = new GregorianCalendar(2016, Calendar.FEBRUARY, 25, 11, 01);  // this is THUR
+        Calendar lastCompleted = new GregorianCalendar(2016, Calendar.FEBRUARY, 23, 11, 02);  // TUES
+        Long prevMS = lastCompleted.getTimeInMillis();
+
+        ArrayList<Integer> testWeekly = new ArrayList<Integer>();
+        testWeekly.add(Calendar.MONDAY);
+        testWeekly.add(Calendar.THURSDAY);
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weekly, testWeekly, prevMS);
+        testRecurrence.setTestTimeMS(testDate.getTimeInMillis());
+        assertTrue("weekly overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+
+    }
+
+
+    @Test
+    public void testWeeklyNotOverdue() {
+        // due Mon/Thurs
+        // set date to Wed, set last completed to Mon
+        Calendar testDate = new GregorianCalendar(2016, Calendar.FEBRUARY, 24);  // this is WED
+        Calendar lastCompleted = new GregorianCalendar(2016, Calendar.FEBRUARY, 22);  // MON
+        Long prevMS = lastCompleted.getTimeInMillis();
+
+        ArrayList<Integer> testWeekly = new ArrayList<Integer>();
+        testWeekly.add(Calendar.MONDAY);
+        testWeekly.add(Calendar.THURSDAY);
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weekly, testWeekly, prevMS);
+        testRecurrence.setTestTimeMS(testDate.getTimeInMillis());
+        assertFalse("weekly should NOT be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+
+    }
+
+    @Test
+    public void testWeeklyWrapOverdue() {
+        // due Mon/Thurs
+        // set date to SAT, set last completed to SUN, previous week
+        Calendar testDate = new GregorianCalendar(2016, Calendar.FEBRUARY, 27);  // this is SAT
+        Calendar lastCompleted = new GregorianCalendar(2016, Calendar.FEBRUARY, 14);  // SUN
+        Long prevMS = lastCompleted.getTimeInMillis();
+
+        ArrayList<Integer> testWeekly = new ArrayList<Integer>();
+        testWeekly.add(Calendar.SATURDAY);
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weekly, testWeekly, prevMS);
+        testRecurrence.setTestTimeMS(testDate.getTimeInMillis());
+        assertTrue("weekly overdue wrapping week should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+
+    }
+
+    @Test
+    public void testMonthlyOverdue() {
+
+    }
+
+    @Test
+    public void testYearlyOverdue() {
+
+    }
+
+    @Test
+    public void testDaysOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        cal.add(Calendar.HOUR, -1);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Days, TEST_ARRAY_1, prevMS);
+        assertTrue("daily overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testDaysNotOverdue() {
+        Calendar cal = Calendar.getInstance();
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Days, TEST_ARRAY_1, prevMS);
+        assertFalse("daily overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testWeeksOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        cal.add(Calendar.HOUR, -1);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weeks, TEST_ARRAY_1, prevMS);
+        assertTrue("weekly overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testWeeksNotOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        cal.add(Calendar.HOUR, 1);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Weeks, TEST_ARRAY_1, prevMS);
+        assertFalse("weekly overdue should not be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testMonthsOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, -1);
+        cal.add(Calendar.HOUR, -1);  //exceed due by 1 hour
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Months, TEST_ARRAY_1, prevMS);
+        assertTrue("monthly overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testMonthsNotOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, -1);
+        cal.add(Calendar.HOUR, 1);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Months, TEST_ARRAY_1, prevMS);
+        assertFalse("monthly overdue should NOT be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testYearsOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -1);
+        cal.add(Calendar.HOUR, -1); //exceed due by 1 hour
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Years, TEST_ARRAY_1, prevMS);
+        assertTrue("yearly overdue should be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
+    @Test
+    public void testYearsNotOverdue() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -1);
+        cal.add(Calendar.HOUR, 1);
+        Long prevMS = cal.getTimeInMillis();
+        Recurrence testRecurrence = new Recurrence(Recurrence.RecurrenceType.Years, TEST_ARRAY_1, prevMS);
+        assertFalse("yearly overdue should NOT be due=" + testRecurrence.dueDeltaMS(), (testRecurrence.dueDeltaMS() < 0));
+    }
+
 }
